13ebbcfb90a5b68e115bcfa1a650ce2b14cf01c5
KeithHHill <keith.h.hill.85@gmail.com>
2018-03-30 10:43:52 -0400

    added config file and imgur upload

--- VideoToGif/VideoToGif.py
+++ VideoToGif/VideoToGif.py
@@ -3,28 +3,35 @@
 
 import os
 from moviepy.editor import *
-import sys
 import glob
+import ConfigParser
+import imgurpython
 
 
+# get items in the config file
 
-# set the input and output path
+config = ConfigParser.ConfigParser()
+myPath = os.path.dirname(os.path.abspath(__file__))
 
-input_path = "E:\Video\VideoToGif\input"
-output_path = "E:\Video\VideoToGif\output"
-output_fps = 15
+config.read(myPath+"\config.ini")
+input_path = config.get('gif','input_path')
+output_path = config.get('gif','output_path')
+output_fps = int(config.get('gif','fps'))
+resize = float(config.get('gif','resize'))
 
-os.chdir(input_path) # move to input folder
-for file in glob.glob("*.mp4") : #loop for each video in the folder
-    print(file + " has been found and will be converted to gif")
+# get imgur credentials and create a client
+client_id = config.get('imgur','client_id')
+client_secret = config.get('imgur','client_secret')
+ImgurClient = imgurpython.ImgurClient(client_id,client_secret)
 
-    #strip the .mp4 off the name and make it gif
-    #file_name = file.strip(".mp4") + ".gif"
+
+def convertVideo(file) :
+    # get a new file name
     file_name = file[:-4] + ".gif"
-    
+  
     # set the clip
     clip = (VideoFileClip(file)
-        .resize(0.25))
+        .resize(resize))
 
 
     # go to output folder and write
@@ -33,3 +40,41 @@ for file in glob.glob("*.mp4") : #loop for each video in the folder
     
     #return to input folder
     os.chdir(input_path)
+    return file_name
+
+
+def uploadToImgur(gif) :
+    os.chdir(output_path) # move to output folder
+
+    # get the file size of the new gif
+    fileinfo = os.stat(gif)
+
+    if fileinfo.st_size > 10000000 : # imgur limits the upload to 10mb
+        print("file is too large and cannot be uploaded to imgur")
+        return None
+
+    else :
+        config = {
+		    'name':  gif,
+		    'title': gif,
+	    }
+        print("Uploading image... ")
+        image = ImgurClient.upload_from_path(gif, config=config, anon=True)
+        return image
+
+
+
+os.chdir(input_path) # move to input folder
+for file in glob.glob("*.mp4") : #loop for each video in the folder
+    print(file + " has been found and will be converted to gif")
+    
+    # convert the video
+    new_gif = convertVideo(file) 
+
+    # upload to imgur
+    image = uploadToImgur(new_gif)
+    
+    if image is not None:
+        print(image['gifv'])
+
+    
